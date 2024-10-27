1f4acead8b91a60b92c8c622ec1e0bdc09923751
Kim Love <kim@kimlove.com>
2024-10-27 10:54:33 +0000

    feat: Display restaurant name in detail card

--- src/components/RestaurantDetails.tsx
+++ src/components/RestaurantDetails.tsx
@@ -4,6 +4,7 @@ import { Card, Container } from "react-bootstrap";
 // we'll move this into a shared types file later
 type RestaurantDetailsProps = {
   restaurant: {
+    name: string;
     details: {
       address: string;
       openingHours: {
@@ -24,7 +25,7 @@ export const RestaurantDetails: React.FC<RestaurantDetailsProps> = ({
     <Container>
       <Card>
         <Card.Body>
-          <Card.Title>Restaurant Details</Card.Title>
+          <Card.Title>{restaurant.name}</Card.Title>
           <Card.Text>Address: {restaurant.details.address}</Card.Text>
           <Card.Text>Review Score: {restaurant.details.reviewScore}</Card.Text>
           <Card.Text>Contact: {restaurant.details.contactEmail}</Card.Text>
