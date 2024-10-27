97713900b9b83081306a50a363459ccb036db11a
Kim Love <kim@kimlove.com>
2024-10-27 14:45:29 +0000

    feat: Added opening hours and linked email address in details component

--- src/components/RestaurantDetails.tsx
+++ src/components/RestaurantDetails.tsx
@@ -11,13 +11,27 @@ export const RestaurantDetails: React.FC<RestaurantDetailsProps> = ({
   restaurant,
 }) => {
   return (
-    <Container className="fade-in">
+    <Container className="mt-4 fade-in">
       <Card>
         <Card.Body>
           <Card.Title>{restaurant.name}</Card.Title>
           <Card.Text>Address: {restaurant.details.address}</Card.Text>
           <Card.Text>Review Score: {restaurant.rating}</Card.Text>
-          <Card.Text>Contact: {restaurant.details.contactEmail}</Card.Text>
+          <Card.Text className="d-flex gap-2">
+            <span>Opening hours: </span>
+            <ul className="list-unstyled">
+              <li>Weekday: {restaurant.details.openingHours.weekday}</li>
+              <li>Weekend: {restaurant.details.openingHours.weekend}</li>
+            </ul>
+          </Card.Text>
+          <Card.Text>
+            Contact:{" "}
+            <strong>
+              <a href={`mailto:${restaurant.details.contactEmail}`}>
+                {restaurant.details.contactEmail}
+              </a>
+            </strong>
+          </Card.Text>
         </Card.Body>
       </Card>
     </Container>
